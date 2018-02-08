3c0fb6adaf5a9bcc5598f2aef149c9c65536a8be
Wellington <wdcunha@gmail.com>
2018-02-07 17:47:11 -0800

    working on transaction type full stack

--- app/controllers/transaction_type_controller.rb
+++ app/controllers/transaction_type_controller.rb
@@ -2,7 +2,7 @@ class TransactionTypeController < ApplicationController
   before_action :find_transacType
 
   def index
-    @transacType = TransactionType.all.order(created_at: :desc)
+    @transacTypes = TransactionType.all.order(created_at: :desc)
   end
 
   def new
@@ -17,6 +17,23 @@ class TransactionTypeController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @transacType.update(transacType_params)
+      redirect_to transaction_type_index_path, success: 'Item updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @transacType.destroy
+    redirect_to transaction_type_index_path, success: 'Item deleted!'
   end
 
   private
